27cd9ccd19a570860ae0cb28d583e15c7e461d4d
Yichun Tsai <ch543ch543@gmail.com>
2020-07-28 21:21:32 +0800

    Updates

---
+++ precache-manifest.f25a8e257546f7ae4ed878599c5c348b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb6136cfd47353b882b4546ba261d48",
+    "url": "/reactRobot/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/reactRobot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "94f00f6e09b12bcf8466",
+    "url": "/reactRobot/static/css/main.e2a6eed5.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/reactRobot/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRobot/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f00f6e09b12bcf8466",
+    "url": "/reactRobot/static/js/main.6a3a6338.chunk.js"
+  },
+  {
+    "revision": "db1bf32b88fcfbd40459",
+    "url": "/reactRobot/static/js/runtime-main.f6b3dffd.js"
+  }
+]);
