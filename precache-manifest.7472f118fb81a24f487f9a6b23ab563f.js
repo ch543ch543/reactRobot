c49c9514b7b663037224eee9f158b35d882dff5a
Yichun Tsai <ch543ch543@gmail.com>
2020-07-30 16:57:55 +0800

    Updates

--- precache-manifest.7472f118fb81a24f487f9a6b23ab563f.js
+++ precache-manifest.7472f118fb81a24f487f9a6b23ab563f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9fb6136cfd47353b882b4546ba261d48",
+    "revision": "bcec644828bfeaee22f40f44885009e6",
     "url": "/reactRobot/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactRobot/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "94f00f6e09b12bcf8466",
+    "revision": "6f0556bcefb0e32d5599",
     "url": "/reactRobot/static/css/main.e2a6eed5.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactRobot/static/js/2.cab51068.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94f00f6e09b12bcf8466",
-    "url": "/reactRobot/static/js/main.6a3a6338.chunk.js"
+    "revision": "6f0556bcefb0e32d5599",
+    "url": "/reactRobot/static/js/main.ac66ab0a.chunk.js"
   },
   {
     "revision": "db1bf32b88fcfbd40459",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactRobot/precache-manifest.f25a8e257546f7ae4ed878599c5c348b.js"
+  "/reactRobot/precache-manifest.7472f118fb81a24f487f9a6b23ab563f.js"
 );
 
 self.addEventListener('message', (event) => {
